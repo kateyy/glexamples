0fe287269d8ee46879874fd41c914d5fe8ec1727
Karsten Tausche <karsten.tausche@student.hpi.uni-potsdam.de>
2015-07-26 19:32:07 +0200

    don't crash if a texture is missing

--- source/antianti/antianti/ImageLoader.cpp
+++ source/antianti/antianti/ImageLoader.cpp
@@ -6,5 +6,8 @@
 QImage loadImage(std::string path)
 {
     QImage image = QImage(QString(path.c_str()));
+    if (image.isNull())
+        return{};
+
     return QGLWidget::convertToGLFormat(image);
 }
